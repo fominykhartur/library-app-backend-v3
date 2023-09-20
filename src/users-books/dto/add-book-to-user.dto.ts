938ed7bb48d25250c7a645474fc075212a665bec
fominykhartur <fominykhartur+GitHub@gmail.com>
2023-09-20 23:28:30 +0300

    добавлена валидация всех dto

--- src/users-books/dto/add-book-to-user.dto.ts
+++ src/users-books/dto/add-book-to-user.dto.ts
@@ -1,14 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
 
 export class AddBookToUser {
   @ApiProperty({
     example: '1',
     description: 'Уникальный идентификатор книги',
   })
+  @IsNumber({}, { message: 'ID книги должен быть числом' })
   bookId: number;
+
   @ApiProperty({
     description: 'Статус книги - прочитан/не прочитано',
     example: false,
   })
+  @IsOptional()
+  @IsBoolean({ message: 'Статус должен быть булевым значением' })
   status?: boolean;
 }
--- src/users-books/dto/change-status.dto.ts
+++ src/users-books/dto/change-status.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class ChangeStatus {
   @ApiProperty({ description: 'id пользователя', example: 1 })
+  @IsNumber({}, { message: 'ID пользователя должен быть числом' })
   id: number;
+
   @ApiProperty({ description: 'id книги', example: 1 })
+  @IsNumber({}, { message: 'ID книги должен быть числом' })
   bookId: number;
 }
